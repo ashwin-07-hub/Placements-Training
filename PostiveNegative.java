b74c93cf8bbc3a641992597d02f85e50b5a38c69
Ashwin-snowking <ashwin.r_25it@kgkite.ac.in>
2026-08-19 09:28:11 +0530

    updated java program

---
+++ PostiveNegative.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class positivenegative {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
+
+            if (n > 0)
+                System.out.println("Positive");
+            else if (n < 0)
+                System.out.println("Negative");
+            else
+                System.out.println("Zero");
+
+            System.out.println();
+        }
+    }
+}
