f45c4be4a67f0b65bc5d815ac27d4c58ca4962d5
Ashwin-snowking <ashwin.r_25it@kgkite.ac.in>
2026-08-23 00:58:41 +0530

    Java updates

---
+++ print1toN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class PrintOneToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ printNto1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class PrintNToOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
